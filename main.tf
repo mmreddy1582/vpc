23ce16459838edac51783c02941debb1ed8fabfe
student1 <noreply@puppet.com>
2020-01-19 00:22:52 +0800

    updated subnets range

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "vpc" {
   usage_nat         = false
   create_route      = true
   vpc_cidr_address  = "192.168.0.0/16"
-  private_subnets   = ["192.168.2.0/24", "192.168.3.0/24", "192.168.4.0/24"]
+  private_subnets   = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
   public_subnets    = ["192.168.8.0/24", "192.168.9.0/24", "192.168.10.0/24"]
  }
 
