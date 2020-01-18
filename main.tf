b899790a4a2384e6a82a74eb37e473f55acae8ed
mmreddy1582 <35883559+mmreddy1582@users.noreply.github.com>
2020-01-18 22:49:31 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ module "vpc" {
   name              = "vpc-mura-non-prod"
   env               = "nonprod"
   usage_nat         = false 
-  create_route      = true
+  create_route      = false
   vpc_cidr_address  = "192.168.0.0/16"
   private_subnets   = ["192.168.2.0/24", "192.168.3.0/24", "192.168.4.0/24", "192.168.5.0/24", "192.168.6.0/24", "192.168.7.0/24"]
   public_subnets    = ["192.168.8.0/24", "192.168.9.0/24", "192.168.10.0/24", "192.168.11.0/24", "192.168.12.0/24", "192.168.13.0/24"]
